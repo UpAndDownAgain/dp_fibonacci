97cc6d8f01ac3b5e28c5d3eba6b2cb6b1c50ee6c
upanddownagain <QMK4nvXv>
2019-12-15 17:47:17 +0100

    styling
    Signed-off-by: upanddownagain <QMK4nvXv>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        Fibonacci fb = new Fibonacci();
+        System.out.println(fb.calcRekTable(100));
     }
 }
